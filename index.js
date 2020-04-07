a87c3c5658972f9ff3fb83906a2b1dabacf7e7ae
Vojtech Janovec <vojtech.janovec@gmail.com>
2020-04-07 14:05:21 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict';
+const BootBot = require('bootbot');
+const config = require('config');
+
+const bot = new BootBot({
+    accessToken: config.get('ACCESS_TOKEN'),
+    verifyToken: config.get('VERIFY_TOKEN'),
+    appSecret: config.get('APP_SECRET')
+  });
+
+bot.on('message', (payload, chat) => {
+  const text = payload.message.text;
+  chat.say(`Echo: ${text}`);
+});
+
+bot.start();
+
+
+
+
+bot.hear(['hello', 'hi', /hey( there)?/i], (payload, chat) => {
+	// Send a text message followed by another text message that contains a typing indicator
+	chat.say('Hello, human friend!').then(() => {
+		chat.say('How are you today?', { typing: true });
+	});
+});
+
+bot.hear(['food', 'hungry'], (payload, chat) => {
+	// Send a text message with quick replies
+	chat.say({
+		text: 'What do you want to eat today?',
+		quickReplies: ['Mexican', 'Italian', 'American', 'Argentine']
+	});
+});
+
+bot.hear(['help'], (payload, chat) => {
+	// Send a text message with buttons
+	chat.say({
+		text: 'What do you need help with?',
+		buttons: [
+			{ type: 'postback', title: 'Settings', payload: 'HELP_SETTINGS' },
+			{ type: 'postback', title: 'FAQ', payload: 'HELP_FAQ' },
+			{ type: 'postback', title: 'Talk to a human', payload: 'HELP_HUMAN' }
+		]
+	});
+});
+
+bot.hear('image', (payload, chat) => {
+	// Send an attachment
+	chat.say({
+		attachment: 'image',
+		url: 'http://example.com/image.png'
+	});
+});
